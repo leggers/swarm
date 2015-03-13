0d9b6d740e286e47c7bd4d46fdb8477c3cb871c2
Lucas S Eggers <l.eggers91@gmail.com>
2015-03-13 13:09:43 -0700

    particle system

--- src/swarm/particles.clj
+++ src/swarm/particles.clj
@@ -3,30 +3,54 @@
         [clojure.core.matrix.operators]))
 (set-current-implementation :vectorz)
 
-(defprotocol Updater
-  (update-position [p]))
+(defprotocol ParticleUpdater
+  (update-position [particle]))
+
+(defprotocol ParticleSystemUpdater
+  (update-all-positions [system]))
 
 (defrecord Particle [position
                      velocity
                      force-accumulator
                      mass]
-  Updater
-  (update-position [p]
-    (assoc p :position (+ (:position p)
+
+  ParticleUpdater
+  (update-position [particle]
+    (assoc particle :position (+ (:position particle)
                           1))))
 
 (defrecord ParticleSystem [particles
                            n-particles
                            simulation-time
                            forces
-                           n-forces])
+                           n-forces]
 
-(def p (->Particle
-         (array [1 2 3])
-         (array [3 6 7])
-         (array [0 0 0])
-         5))
+  ParticleSystemUpdater
+  (update-all-positions [system]
+    (assoc system :particles (map update-position
+                                  (:particles system)))))
+
+(defn make-particle [x y z]
+  (->Particle
+    (array [x y z])
+    (array [0 0 0])
+    (array [0 0 0])
+    5))
+
+(defn init-particle-system [n]
+  (->ParticleSystem
+    (take n (repeat (make-particle
+                      (rand)
+                      (rand)
+                      (rand))))
+    n
+    0
+    []
+    0))
+
+(def p (make-particle 5 7 9))
+
+(def sys (init-particle-system 5))
 
 (defn -main []
-  (println p)
-  (println (update-position p)))
+  (println sys))
