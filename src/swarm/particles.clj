252c38f8dc41f8795d7434e3df593e275641d9ef
Lucas S Eggers <l.eggers91@gmail.com>
2015-03-13 11:48:19 -0700

    particle system

--- src/swarm/particles.clj
+++ src/swarm/particles.clj
@@ -2,7 +2,22 @@
   (:use [clojure.core.matrix]))
 (set-current-implementation :vectorz)
 
+(defrecord Particle [position
+                     velocity
+                     force-accumulator
+                     mass])
+
+(defrecord ParticleSystem [particles
+                           n-particles
+                           simulation-time
+                           forces
+                           n-forces])
+
 (def a (array [1 2 3]))
 
 (defn -main []
-  (println a))
+  (println (->Particle
+               (array [1 2 3])
+               (array [3 6 7])
+               (array [0 0 0])
+               5)))
