d6de8b6124b58e5851f2048a9161fa71f02648f5
Lucas S Eggers <l.eggers91@gmail.com>
2015-03-13 12:08:21 -0700

    added quil to play with drawing

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [net.mikera/core.matrix "0.33.2"]
-                 [net.mikera/vectorz-clj "0.29.0"]])
+                 [net.mikera/vectorz-clj "0.29.0"]
+                 [quil "2.2.5"]])
